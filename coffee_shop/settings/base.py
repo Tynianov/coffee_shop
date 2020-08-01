cb590050fc7f0312d694c76718693d49009e613b
tynianov <saficty@gmail.com>
2020-08-01 16:56:34 +0300

    add django-jet admin

--- coffee_shop/settings/base.py
+++ coffee_shop/settings/base.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -87,6 +88,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
