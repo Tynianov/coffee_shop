feebc563b233b98bdc46b8054e5cbe8d41e3072d
Alexander <alex.sivkov@faifly.com>
2020-08-20 23:01:13 +0300

    add django-fcm

---
+++ coffee_shop/settings/local_example.py
@@ -0,0 +1,33 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_name',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'host',
+        'PORT': '3306',
+        'OPTIONS': {
+            # 'init_command': 'SET storage_engine=InnoDB; SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+
+
+PROTOCOL = 'http'
+
+SECRET_KEY = 'your secret key'
+
+FCM_DJANGO_SETTINGS = {
+    "FCM_SERVER_KEY": "api key goes here"
+
+}
