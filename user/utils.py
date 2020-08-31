f5ac8152fcbd509e00f902b37354d4c42e4d0b1e
Sasha Tynianov <saficty@gmail.com>
2020-08-31 23:37:06 +0300

    send push notification on user qr code scan

---
+++ user/utils.py
@@ -0,0 +1,31 @@
+import logging
+from django.conf import settings
+from fcm_django.models import FCMDevice
+
+
+logger = logging.getLogger('db')
+
+
+def send_push_notification(user, title, data):
+    if not settings.FCM_SERVER_KEY:
+        logger.error("No FCM key specified")
+        return
+
+    device = FCMDevice.objects.filter(user=user).first()
+
+    if not device:
+        logger.error(f"Device not found. User id: {user.id}")
+        return
+
+    message_data = {
+        'title': title,
+        'data': data,
+        'api_key': settings.FCM_SERVER_KEY
+    }
+    if device.type == 'android':
+        message_data.update({
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK'
+        })
+
+    device.send_message(**message_data)
+    logger.info("Push send")
