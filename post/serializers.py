12dc8add98d5f706d2da9a4c3981b853fa2da657
Sasha Tynianov <saficty@gmail.com>
2020-09-25 15:46:22 +0300

    update post view serializer

--- post/serializers.py
+++ post/serializers.py
@@ -6,34 +6,17 @@ from .models import Post, PostConfig
 
 class PostListSerializer(serializers.ModelSerializer):
     short_description = serializers.SerializerMethodField()
-    first_image = serializers.SerializerMethodField()
     created = serializers.SerializerMethodField()
+    post_images = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'short_description', 'first_image', 'content', 'created']
+        fields = ['id', 'title', 'short_description', 'post_images', 'content', 'created']
 
     def get_short_description(self, obj):
         if len(obj.content) < 128:
             return obj.content
         return f'{obj.content[:128]}...'
-
-    def get_first_image(self, obj):
-        if obj.images:
-            return get_absolute_url(obj.images.all().first().image.url)
-        return None
-
-    def get_created(self, obj):
-        return obj.created.strftime("%d/%m/%Y")
-
-
-class PostDetailsSerializer(serializers.ModelSerializer):
-    post_images = serializers.SerializerMethodField()
-    created = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Post
-        fields = '__all__'
 
     def get_post_images(self, obj):
         if obj.images:
--- post/views.py
+++ post/views.py
@@ -3,18 +3,14 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
 
-from .serializers import PostListSerializer, PostDetailsSerializer, PostConfigSerializer
+from .serializers import PostListSerializer, PostConfigSerializer
 from .models import Post, PostConfig
 
 
 class PostViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     queryset = Post.objects.active()
     lookup_field = 'id'
-
-    def get_serializer_class(self):
-        if self.kwargs.get(self.lookup_field, None):
-            return PostDetailsSerializer
-        return PostListSerializer
+    serializer_class = PostListSerializer
 
 
 class PostConfigView(APIView):
