d9ee9b003f7bb976eaa4e90b8264d4f5b746c4e2
cr0manty <aappland@gmail.com>
2020-10-02 21:43:25 +0300

    delete unused configs

--- coffee_shop/settings/production.example.py
+++ coffee_shop/settings/production.example.py
@@ -6,19 +6,18 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': 'db_name',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'host',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': 'SET default_storage_engine=InnoDB',
-            'charset': 'utf8',
+            # 'init_command': 'SET storage_engine=InnoDB; SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset': 'utf8mb4',
             'use_unicode': True,
         },
-    }
+    },
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -29,4 +28,11 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
 
-PROTOCOL = 'https'
+PROTOCOL = 'http'
+
+SECRET_KEY = 'your secret key'
+
+TWILIO_SID = 'twilio sid'
+TWILIO_AUTH_TOKEN = 'twilio key'
+TWILIO_PHONE_NUMBER = 'twilio phone number'
+SENTRY_DSN = 'sentry dsn'
