5cea5c845f502d5d4440215fed646a31a699f9b6
Sasha Tynianov <saficty@gmail.com>
2020-11-02 23:12:37 +0200

    update print message

--- voucher/management/commands/recreate_voucher_qr_code.py
+++ voucher/management/commands/recreate_voucher_qr_code.py
@@ -9,6 +9,6 @@ class Command(BaseCommand):
         for voucher in Voucher.objects.not_scanned():
             try:
                 recreate_model_qr_code(voucher)
-                print(f'QR code successfully recreated for user with id {voucher.id}')
+                print(f'QR code successfully recreated for voucher with id {voucher.id}')
             except Exception as e:
                 print(f'Error occurred while updating qr code for user {voucher.id}, {e}')
